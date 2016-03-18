01a73fb98e474e347958b23be71b33af83e08108
yaol9 <yaoliang@gmail.com>
2016-03-18 11:07:22 +1100

    Create 268_missing_number.js

---
+++ 268_missing_number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    nums.sort(function(a,b){return a-b});
+    for(var i=0;i<nums.length;i++){
+        if(nums[i]!=i){
+            return i;
+        }
+    }
+    return nums.length;
+};
